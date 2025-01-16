6b0ef2df9f3b64789368d5735973d8fd2d90bdb8
Ritesh-Borale <riteshborale7@gmail.com>
2025-01-16 15:15:00 +0530

    HomePage Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import AppRoutes from './routes/Approutes'
 import { UserProvider } from "./context/user.context"
+import './App.css'
+import { Toaster } from 'react-hot-toast';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <UserProvider>
-      <AppRoutes/>
+
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          success: {
+            theme: {
+              primary: '#4aed88',
+            },
+          },
+        }}
+      />
+      <AppRoutes />
+    
     </UserProvider>
   )
 }
--- frontend/src/routes/Approutes.jsx
+++ frontend/src/routes/Approutes.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Routes, BrowserRouter, Route } from "react-router-dom"
 import Login from "../screens/Login"
 import Register from "../screens/Register"
-// import Home from "../screens/Home"
+import Home from "../screens/Home"
 import UserAuth from '../auth/UserAuth'
 
 function AppRoutes() {
@@ -10,7 +10,7 @@ function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* <Route path="/" element={<UserAuth><Home/></UserAuth>}/> */}
+                <Route path="/" element={<UserAuth><Home/></UserAuth>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
                 {/* <Route path="/project" element={<UserAuth><Project/></UserAuth>}/> */}
